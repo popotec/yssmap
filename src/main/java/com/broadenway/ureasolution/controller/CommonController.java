041e264ee190434d3f7a05054d327ccaccb854a6
jerry92k <jens92.k@gmail.com>
2021-11-25 09:14:22 +0900

    feat: 검색엔진 노출 추가

--- src/main/java/com/broadenway/ureasolution/controller/CommonController.java
+++ src/main/java/com/broadenway/ureasolution/controller/CommonController.java
@@ -15,7 +15,13 @@ public class CommonController {
 			"Disallow: /\n" +
 			"User-agent: Googlebot\n" +
 			"Allow: /\n" +
+			"Sitemap: https://yssmap.com/sitemap.xml\n" +
+			"User-agent: Daum\n" +
+			"Allow: /\n" +
+			"Sitemap: https://yssmap.com/sitemap.xml\n" +
 			"User-agent: Yetibot\n" +
-			"Allow: /\n";
+			"Allow: /\n" +
+			"Sitemap: https://yssmap.com/sitemap.xml\n" +
+			"DaumWebMasterTool:95d3266fc401ba3bacb41a69b11f55a404664144e7609a814f68c0890b147c46:AfWVm7J7RFpH4KiqxuBceA==";
 	}
 }
