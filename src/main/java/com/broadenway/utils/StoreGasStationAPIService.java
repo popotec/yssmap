fdbb07cbfbee87e78869fe88a3ac43fcaeb0329a
jerry92k <jens92.k@gmail.com>
2021-12-15 20:06:12 +0900

    feat: API 페이징해서 호출

--- src/main/java/com/broadenway/utils/StoreGasStationAPIService.java
+++ src/main/java/com/broadenway/utils/StoreGasStationAPIService.java
@@ -26,6 +26,8 @@ import com.broadenway.utils.gasdomain.GasStationRepository;
 public class StoreGasStationAPIService implements ApplicationRunner {
 
 	private static final String BASE_REQUEST_URL = "https://api.odcloud.kr/api/uws/v1/inventory";
+	private static final int INIT_PAGE = 1;
+	private static final int INIT_PER_PAGE = 200;
 
 	@Value("${dataportal.servicekey}")
 	private String serviceKey;
@@ -39,16 +41,15 @@ public class StoreGasStationAPIService implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		Map responseData = requestAPIData();
-		List<GasStation> gasStations = convertDataToGasStationEntity(responseData);
+		List<GasStation> gasStations = getStationsFromAPI();
 		storeDatabase(gasStations);
 	}
 
 	private void storeDatabase(List<GasStation> gasStations) {
-		for(GasStation gasStation : gasStations){
+		for (GasStation gasStation : gasStations) {
 			Optional<GasStation> findGasStation = gasStationRepository.findByStationCode(
 				gasStation.getStationCode());
-			if(findGasStation.isPresent()){
+			if (findGasStation.isPresent()) {
 				findGasStation.get().update(gasStation);
 				continue;
 			}
@@ -56,12 +57,29 @@ public class StoreGasStationAPIService implements ApplicationRunner {
 		}
 	}
 
-	private Map requestAPIData() {
+	private List<GasStation> getStationsFromAPI() {
 
+		int page = INIT_PAGE;
+
+		Map result = requestAPICall(page);
+		int totalCount = (Integer)result.get("totalCount");
+		List<GasStation> gasStations = convertDataToGasStationEntity(result);
+
+		if (totalCount<=INIT_PER_PAGE) {
+			return gasStations;
+		}
+
+		int totalPage = (int)Math.ceil((double)totalCount / 200);
+		for (int i = 2; i <= totalPage; i++) {
+			Map nextResult = requestAPICall(i);
+			gasStations.addAll(convertDataToGasStationEntity(nextResult));
+		}
+
+		return gasStations;
+	}
+
+	private Map requestAPICall(int page) {
 		RestTemplate restTemplate = new RestTemplate();
-
-		System.out.println("servicekey");
-		System.out.println(serviceKey);
 
 		HttpHeaders httpHeaders = new HttpHeaders();
 		httpHeaders.set(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
@@ -70,29 +88,30 @@ public class StoreGasStationAPIService implements ApplicationRunner {
 
 		HttpEntity<?> httpEntity = new HttpEntity<>(httpHeaders);
 
-		String page="1";
-		String perPage="200";
-		String urlBuilder = BASE_REQUEST_URL+"?page="+page+"&perPage="+perPage+"&serviceKey="+serviceKey;
+		String urlBuilder = BASE_REQUEST_URL +
+			"?page=" + page +
+			"&perPage=" + INIT_PER_PAGE +
+			"&serviceKey=" + serviceKey;
 
+		System.out.println(urlBuilder);
 		return restTemplate.exchange(urlBuilder, HttpMethod.GET, httpEntity, Map.class).getBody();
 	}
-
 
 	private List<GasStation> convertDataToGasStationEntity(Map responseBody) {
 		List<Map> extractedData = (ArrayList)responseBody.get("data");
 
 		return extractedData.stream().map(data ->
-				new GasStation(
-					(String)data.get(ResponseFieldName.STATION_CODE.getName()),
-					(String)data.get(ResponseFieldName.NAME.getName()),
-					(String)data.get(ResponseFieldName.ADDRESS.getName()),
-					(String)data.get(ResponseFieldName.TEL_NO.getName()),
-					(String)data.get(ResponseFieldName.OPENNING_HOURS.getName()),
-					String.valueOf(data.get(ResponseFieldName.STOCSK.getName())),
-					(String)data.get(ResponseFieldName.PRICE.getName()),
-					(String)data.get(ResponseFieldName.LATITUDE.getName()),
-					(String)data.get(ResponseFieldName.LONGITUDE.getName()),
-					(String)data.get(ResponseFieldName.STD_DT.getName())
-				)).collect(Collectors.toList());
+			new GasStation(
+				(String)data.get(ResponseFieldName.STATION_CODE.getName()),
+				(String)data.get(ResponseFieldName.NAME.getName()),
+				(String)data.get(ResponseFieldName.ADDRESS.getName()),
+				(String)data.get(ResponseFieldName.TEL_NO.getName()),
+				(String)data.get(ResponseFieldName.OPENNING_HOURS.getName()),
+				String.valueOf(data.get(ResponseFieldName.STOCSK.getName())),
+				(String)data.get(ResponseFieldName.PRICE.getName()),
+				(String)data.get(ResponseFieldName.LATITUDE.getName()),
+				(String)data.get(ResponseFieldName.LONGITUDE.getName()),
+				(String)data.get(ResponseFieldName.STD_DT.getName())
+			)).collect(Collectors.toList());
 	}
 }
