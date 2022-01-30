fff7006fd7d81d324588514b25aabafdbc52b060
jerry92k <jens92.k@gmail.com>
2022-01-30 22:19:45 +0900

    docs : 배치 처리를 멀티스레드로 변경할 경우 주의사항 작성

--- yssmap-batch/src/main/java/yssmap/batch/job/chunk/GasStationApiReader.java
+++ yssmap-batch/src/main/java/yssmap/batch/job/chunk/GasStationApiReader.java
@@ -14,7 +14,13 @@ public class GasStationApiReader implements ItemReader<List<GasStationDto>> {
 	private static final Logger logger = LoggerFactory.getLogger("file");
 	private final GasStationAPIService storeGasStationAPIService;
 	private final int maxPage;
-	private int page = 0; // TODO: ThreadLocal로 변경
+
+	/*
+	 * 멀티스레드로 변경할 경우 page에 대한 연산이 thread-safe 하지 않으므로 ItemReader -> SynchronizedItemStreamReaderBuilder 로
+	 * 변경하여 사용해야 한다.
+	 * 각 스레드별 page 번호를 다르게 저장하면 안되고, 같은 page 번호를 공유하며 동기화해서 사용해야 하므로 ThreadLocal은 사용하면 안된다.
+ 	 */
+	private int page = 0;
 
 	public GasStationApiReader(GasStationAPIService storeGasStationAPIService) {
 		this.storeGasStationAPIService = storeGasStationAPIService;
@@ -24,6 +30,7 @@ public class GasStationApiReader implements ItemReader<List<GasStationDto>> {
 	@Override
 	public List<GasStationDto> read() {
 		page++;
+
 		if (page > maxPage) {
 			return null;
 		}
