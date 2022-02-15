dfdce35b82a15096ed9de8620db91c434329c165
jerry92k <jens92.k@gmail.com>
2022-02-15 16:29:06 +0900

    test: jenkins 테스트용 커밋

---
+++ yssmap-batch/src/test/java/yssmap/stationapi/JenkinsTest.java
@@ -0,0 +1,11 @@
+package yssmap.stationapi;
+
+import org.junit.jupiter.api.Test;
+
+public class JenkinsTest {
+
+	@Test
+	void test(){
+		System.out.println("jenkins github hook 테스트용 code");
+	}
+}
